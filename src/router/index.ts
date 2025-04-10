de22faf3e019bfee2ff01e2d395ea75d25b7168e
stu.chen <best_css@foxmail.com>
2025-04-10 11:03:11 +0800

    init

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+﻿import { createRouter, createWebHistory } from 'vue-router'
+import dataCenter from '../view/data-center.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/data-center',
+  },
+  {
+    path: '/data-center',
+    name: 'data-center',
+    component: dataCenter,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
---
+++ src/view/data-center.vue
@@ -0,0 +1,8 @@
+﻿<script setup lang="tsx">
+</script>
+
+<template>
+  <div>datacenter view</div>
+</template>
+
+<style lang="css" scoped></style>
